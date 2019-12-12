26556dfcde197a3503f4d2735ccb8c4c70074bc4
CiGit-Franck <franck.thery@gmail.com>
2019-12-12 18:25:28 +0100

    first commit

---
+++ src/test/java/fty/briefs/starwars/spring/StarWarsSpring/StarWarsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fty.briefs.starwars.spring.StarWarsSpring;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+class StarWarsSpringApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+}
