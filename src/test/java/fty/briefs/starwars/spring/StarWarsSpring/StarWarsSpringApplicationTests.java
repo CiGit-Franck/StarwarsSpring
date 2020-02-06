638c262bcfeda1a9e195dd7fca3234e4f49a8ee5
CiGit-Franck <franck.thery@gmail.com>
2020-02-06 12:22:44 +0100

    daily commit

--- src/test/java/fty/briefs/starwars/spring/StarWarsSpring/StarWarsSpringApplicationTests.java
+++ src/test/java/fty/briefs/starwars/spring/StarWarsSpring/StarWarsSpringApplicationTests.java
@@ -1,13 +1,27 @@
 package fty.briefs.starwars.spring.StarWarsSpring;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StarWarsSpringApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
+    
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getPlanets() {
+        // When retrieving planets from /api/starwars/planets
+        List<?> planets = this.restTemplate.getForObject("/api/starwars/planets", List.class);
+
+        // Then a non null list should be returned
+        assertThat(planets).isNotNull();
+    }
 }
